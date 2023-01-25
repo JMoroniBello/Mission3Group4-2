f6d7ef3bc846d6767d0beea6ee18ef8edbe6d6a8
Josh Bello <superturboturtle@gmail.com>
2023-01-25 13:47:02 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mission3Group4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Welcome to tic-tac-toe!\n Player 1 is X\n Player 2 is O");
+            // array to hold player choices
+            char[] choices = new char[9] {'1','2','3','4','5','6','7','8', '9'};
+
+            int i = 2;
+            char[] placeHolder = new char[2] { 'X', 'O' };
+            int res = 0;
+            int p = 0;
+            //loop to continue game untill draw or win
+            supportClass.PrintBoard(choices);
+            while (res ==0)
+            {
+                p = i % 2;
+                Console.WriteLine("Player " +  (p+1) + " (" + placeHolder[p] + ") , where would you like play?");
+                string Place = Console.ReadLine();
+                //put placeholder in array
+                choices[Int32.Parse(Place)-1] = placeHolder[p];
+                //call supporting class methods to print board and any results
+                supportClass.PrintBoard(choices);
+                res = supportClass.Result(choices);
+                i++;
+            }
+            if (res == -1)
+                Console.WriteLine("The result of the game is a draw!");
+            else
+                Console.WriteLine("Player " + (p+1) + " (" + placeHolder[p] + ") Won! Good Game!");
+                     
+        }
+    }
+}
---
+++ supportClass.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission3Group4_2
+{
+    public class supportClass
+    {
+
+
+        public static void PrintBoard(char[] choices)
+        {
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  " + choices[0] + "  |  " + choices[1] + "  |  " + choices[2] + " ");
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  " + choices[3] + "  |  " + choices[4] + "  |  " + choices[5] + " ");
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  " + choices[6] + "  |  " + choices[7] + "  |  " + choices[8] + " ");
+
+            Console.WriteLine("     |     |      ");
+
+        }
+
+
+
+        public static int Result(char[] choices)
+
+        {
+
+            if (choices[0] == choices[1] && choices[2] == choices[1])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[3] == choices[4] && choices[5] == choices[4])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[6] == choices[7] && choices[8] == choices[7])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[0] == choices[3] && choices[6] == choices[3])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[1] == choices[4] && choices[7] == choices[4])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[2] == choices[5] && choices[8] == choices[5])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[0] == choices[4] && choices[8] == choices[4])
+
+            {
+
+                return 1;
+
+            }
+
+            else if (choices[2] == choices[4] && choices[6] == choices[4])
+
+            {
+
+                return 1;
+
+            }
+
+            else if ((choices[0] == 'X' | choices[0] == 'O') && (choices[1] == 'X' | choices[1] == 'O') && (choices[2] == 'X' | choices[2] == 'O') && (choices[3] == 'X' | choices[3] == 'O') && (choices[4] == 'X' | choices[4] == 'O') && (choices[5] == 'X' | choices[5] == 'O') && (choices[6] == 'X' | choices[6] == 'O') && (choices[7] == 'X' | choices[7] == 'O') && (choices[8] == 'X' | choices[8] == 'O'))
+
+            {
+
+                return -1;
+
+            }
+
+            else
+
+            {
+
+                return 0;
+
+            }
+        }
+    }
+}
